8c1257d4373fefc98e3244fcf78c6ae3e85fa494
Yaroslav Kurilin <yaroslav.kurilin.z@gmail.com>
2022-04-22 13:49:00 +0300

    Add default id for 'unsorted group'.

--- controllers/GroupController.js
+++ controllers/GroupController.js
@@ -3,7 +3,7 @@ const { validationResult } = require("express-validator");
 
 
 const get_groups = async(req, res) => {
-  const defaultItem = {name: 'unsorted', readOnly: true, description: ''}
+  const defaultItem = {name: 'unsorted', readOnly: true, description: '', _id:'00000000'}
   const resData = await GroupsDb.find().where('userId').equals(req.user._id).sort({ name: 1 });
   if (resData) res.send([...resData, defaultItem]);
 }
